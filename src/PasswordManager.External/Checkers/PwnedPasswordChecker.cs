3c5ae5e3fab289a078518e378fee8b4a1824ff17
Maxim Shapovalov <mrypdm@gmail.com>
2025-03-16 19:34:49 +0300

    external pasword checkers

---
+++ src/PasswordManager.External/Checkers/PwnedPasswordChecker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Flurl.Http;
+
+using PasswordManager.Abstractions;
+
+namespace PasswordManager.External.Checkers;
+
+/// <summary>
+/// Сheck password compromise with haveibeenpwned
+/// </summary>
+public class PwnedPasswordChecker : IPasswordChecker
+{
+    public const string UrlPrefix = "https://api.pwnedpasswords.com/range";
+
+    /// <inheritdoc/>
+    public async Task<PasswordCheckStatus> CheckPasswordAsync(string password, CancellationToken token)
+    {
+        var hash = Convert.ToHexString(SHA1.HashData(Encoding.UTF8.GetBytes(password)));
+        var result = await GetCompomisedHashes(hash[..5], token);
+
+        return result.Contains(hash[5..], StringComparison.OrdinalIgnoreCase)
+            ? new PasswordCheckStatus(PasswordCompromisation.Compromised, PasswordStrength.VeryLow)
+            : new PasswordCheckStatus(PasswordCompromisation.NotCompromised, PasswordStrength.Unknown);
+    }
+
+    /// <summary>
+    /// Returns suffixes of hashes (SHA1) of compomised passwords
+    /// </summary>
+    /// <param name="prefix">Prefix (5-digits) of hash</param>
+    /// <param name="token">Cancellation token</param>
+    /// <returns>Suffixes of compromised passwords</returns>
+    private static Task<string> GetCompomisedHashes(string prefix, CancellationToken token)
+    {
+        return $"{UrlPrefix}/{prefix}".GetStringAsync(cancellationToken: token);
+    }
+}
---
+++ src/PasswordManager.External/Checkers/ZxcvbnPasswordChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using PasswordManager.Abstractions;
+
+namespace PasswordManager.External.Checkers;
+
+/// <summary>
+/// Password checker by Zxcvbn method
+/// </summary>
+public class ZxcvbnPasswordChecker : IPasswordChecker
+{
+    /// <inheritdoc/>
+    public Task<PasswordCheckStatus> CheckPasswordAsync(string password, CancellationToken token)
+    {
+        var strength = (PasswordStrength)Zxcvbn.Core.EvaluatePassword(password).Score;
+        return Task.FromResult(new PasswordCheckStatus(PasswordCompromisation.Unknown, strength));
+    }
+}
