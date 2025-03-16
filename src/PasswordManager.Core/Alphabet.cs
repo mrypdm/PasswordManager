50ed4dec669e5262add4d349adff034ac1e5ded8
Maxim Shapovalov <mrypdm@gmail.com>
2025-03-16 19:33:48 +0300

    alphabet and password generator

---
+++ src/PasswordManager.Core/Alphabet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using PasswordManager.Abstractions;
+
+namespace PasswordManager.Core;
+
+/// <inheritdoc />
+public class Alphabet : IAlphabet
+{
+    public const string LowerLetters = "qwertyuiopasdfghjklzxcvbnm";
+
+    public const string UpperLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+    public const string Numbers = "0123456789";
+
+    public const string Charecters = "`~!@#$%^&*+-/.,\\{}[]();:?<>\"'_";
+
+    private readonly StringBuilder _alphabetBuilder = new();
+
+    /// <inheritdoc />
+    public HashSet<char> GetCharacters()
+    {
+        return [.. _alphabetBuilder.ToString().ToCharArray()];
+    }
+
+    /// <summary>
+    /// Append <see cref="LowerLetters"/> to alphabet
+    /// </summary>
+    public Alphabet WithLowerLetters()
+    {
+        _alphabetBuilder.Append(LowerLetters);
+        return this;
+    }
+
+    /// <summary>
+    /// Append <see cref="UpperLetters"/> to alphabet
+    /// </summary>
+    public Alphabet WithUpperLetters()
+    {
+        _alphabetBuilder.Append(UpperLetters);
+        return this;
+    }
+
+    /// <summary>
+    /// Append <see cref="Numbers"/> to alphabet
+    /// </summary>
+    public Alphabet WithNumbersLetters()
+    {
+        _alphabetBuilder.Append(Numbers);
+        return this;
+    }
+
+    /// <summary>
+    /// Append intersection of <paramref name="allowedCharacters"/> and <see cref="Charecters"/> to alphabet
+    /// </summary>
+    public Alphabet WithCharacters(string allowedCharacters = Charecters)
+    {
+        _alphabetBuilder.Append([.. allowedCharacters.Intersect(Charecters)]);
+        return this;
+    }
+}
---
+++ src/PasswordManager.Core/PasswordGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+
+using PasswordManager.Abstractions;
+
+namespace PasswordManager.Core;
+
+/// <inheritdoc />
+public class PasswordGenerator : IPasswordGenerator
+{
+    /// <inheritdoc />
+    public string GeneratePassword(IAlphabet alphabet, int length)
+    {
+        return new(RandomNumberGenerator.GetItems<char>(alphabet.GetCharacters().ToArray(), length));
+    }
+
+    /// <summary>
+    /// Generates password
+    /// </summary>
+    public string GeneratePassword(int length,
+        bool useLowerLetters = true,
+        bool useUpperLetters = true,
+        bool useNubmers = true,
+        bool useCharacters = true,
+        string allowedCharacters = Alphabet.Charecters)
+    {
+        var alphabet = new Alphabet();
+
+        if (useLowerLetters)
+        {
+            alphabet.WithLowerLetters();
+        }
+
+        if (useUpperLetters)
+        {
+            alphabet.WithUpperLetters();
+        }
+
+        if (useNubmers)
+        {
+            alphabet.WithNumbersLetters();
+        }
+
+        if (useCharacters)
+        {
+            alphabet.WithCharacters(allowedCharacters);
+        }
+
+        return GeneratePassword(alphabet, length);
+    }
+}
