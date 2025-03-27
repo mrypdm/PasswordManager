a74353ada8b51f79ff1bcf8801b80ce305d08fe6
Maxim Shapovalov <mrypdm@gmail.com>
2025-03-27 20:21:38 +0300

    master key factory

---
+++ src/PasswordManager.Abstractions/IMasterKeyFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace PasswordManager.Abstractions;
+
+/// <summary>
+/// Factory for master key
+/// </summary>
+public interface IMasterKeyFactory
+{
+    /// <summary>
+    /// Create master key by master password
+    /// </summary>
+    /// <remarks>
+    /// The output key is 256 bits in size.
+    /// </remarks>
+    public byte[] CreateMasterKey(string masterPassword);
+}
