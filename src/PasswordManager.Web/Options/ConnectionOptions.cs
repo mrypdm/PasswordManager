87ed0309c197e6118ce36e641c26d2ea70c9c1a1
Maxim Shapovalov <mrypdm@gmail.com>
2025-04-03 16:35:29 +0300

    connection options

---
+++ src/PasswordManager.Web/Options/ConnectionOptions.cs
@@ -0,0 +1,12 @@
+namespace PasswordManager.Web.Options;
+
+/// <summary>
+/// Options for connection
+/// </summary>
+public class ConnectionOptions
+{
+    /// <summary>
+    /// If password manager work under a proxy
+    /// </summary>
+    public bool IsProxyUsed { get; set; }
+}
