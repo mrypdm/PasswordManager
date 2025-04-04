fe7b784b8d78d70159a02fced273602ac58b57f4
Maxim Shapovalov <mrypdm@gmail.com>
2025-04-04 14:15:41 +0300

    password generator as layout and seperate page

---
+++ src/PasswordManager.Web/Pages/Generator.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PasswordManager.Web.Pages;
+
+/// <summary>
+/// Password generator page
+/// </summary>
+public class GeneratorModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
