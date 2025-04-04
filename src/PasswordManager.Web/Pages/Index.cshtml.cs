7666c2a3e50fa4e1cdceee13012050a37fde667b
Maxim Shapovalov <mrypdm@gmail.com>
2025-04-04 13:04:15 +0300

    empty index

--- src/PasswordManager.Web/Pages/Index.cshtml.cs
+++ src/PasswordManager.Web/Pages/Index.cshtml.cs
@@ -4,15 +4,7 @@ namespace PasswordManager.Web.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
     public void OnGet()
     {
-
     }
 }
